aa6ac95c74a2a3c1cbe4660558de7b2ac3ac4983
Patrick Kalita <pkalita@lbl.gov>
2022-10-13 16:31:34 -0700

    Update project metadata fields to match cookiecutter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,8 @@
-import { useState } from 'react'
-import { saveAs } from 'file-saver'
-import schema from './schema.json'
-import './App.css'
-import { useForm } from 'react-hook-form';
-import { ajvResolver } from '@hookform/resolvers/ajv';
-import { pickBy } from 'lodash'
-import { MantineProvider, TextInput, Button, Box } from '@mantine/core';
-
-// These fields violate Ajv's default strict mode setting
-delete schema['metamodel_version'];
-delete schema['version'];
+import { useState } from 'react';
+import { MantineProvider, Box } from '@mantine/core';
+import ProjectDetailsForm from './components/ProjectDetailsForm';
 
 function App() {
-  const { 
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({
-    resolver: async (data, context, options) => {
-      // you can debug your validation schema here
-      const cleanData = pickBy(data)
-      // console.log('formData', cleanData)
-      // console.log('validation result', await ajvResolver(schema)(cleanData, context, options))
-      return ajvResolver(schema)(cleanData, context, options)
-    },
-  })
   const [filename, setFilename] = useState('')
 
   const handleGenerate = async (formData) => {
@@ -41,28 +19,11 @@ function App() {
     saveAs(blob, filename)
     setFilename(filename)
   }
-
+  
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Box sx={{ maxWidth: 300 }} mx="auto">
-        <form onSubmit={handleSubmit(handleGenerate)}>
-          <TextInput 
-            label='Name'
-            error={errors.name?.message}
-            {...register('name')}
-          />
-          <TextInput 
-            label='Description'
-            error={errors.description?.message}
-            {...register('description')}
-          />
-          <TextInput
-            label='Author'
-            error={errors.author?.message}
-            {...register('author')}
-          />
-          <Button type="submit" mt="md">Submit</Button>
-        </form>
+        <ProjectDetailsForm onSubmit={handleGenerate} />
       </Box>
 
       {filename && (
---
+++ frontend/src/components/ProjectDetailsForm.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { ajvResolver } from '@hookform/resolvers/ajv';
+import { pickBy } from 'lodash'
+import { TextInput, Button, Title, Box, NativeSelect, Textarea } from '@mantine/core';
+
+import schema from '../schema.json'
+
+// These fields violate Ajv's default strict mode setting
+delete schema['metamodel_version'];
+delete schema['version'];
+
+function ProjectDetailsForm({ onSubmit }) {
+  const { 
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: async (data, context, options) => {
+      // you can debug your validation schema here
+      const cleanData = pickBy(data)
+      // console.log('formData', cleanData)
+      // console.log('validation result', await ajvResolver(schema)(cleanData, context, options))
+      return ajvResolver(schema)(cleanData, context, options)
+    },
+  })
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box mb="md">
+        <Title order={3}>Author</Title>
+        <TextInput 
+          label='Name'
+          error={errors.full_name?.message}
+          placeholder='Your Name'
+          {...register('full_name')}
+        />
+        <TextInput 
+          label='Email'
+          error={errors.email?.message}
+          placeholder='you@example.com'
+          {...register('email')}
+        />
+        <TextInput
+          label='GitHub username or organization'
+          error={errors.github_org?.message}
+          {...register('github_org')}
+        />
+      </Box>
+      
+      <Box mb="md">
+        <Title order={3}>Project</Title>
+        <TextInput 
+          label='Name'
+          error={errors.project_name?.message}
+          placeholder='my-schema'
+          {...register('project_name')}
+        />
+        <Textarea 
+          label='Description'
+          error={errors.project_description?.message}
+          {...register('project_description')}
+        />
+        <NativeSelect 
+          label='License'
+          error={errors.license?.message}
+          data={schema.$defs.Licenses.enum}
+          {...register('license')}
+        />
+      </Box>
+      
+      <Button type="submit">Submit</Button>
+    </form>
+  )
+}
+
+export default ProjectDetailsForm
