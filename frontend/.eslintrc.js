37a9c4798bd7daa008a68afbf43da900da81d290
Patrick Kalita <pkalita@lbl.gov>
2022-10-20 09:48:50 -0700

    Move API call up one level to prevent double call on mount

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -23,5 +23,6 @@ module.exports = {
         "react": {
             "version": "detect"
         }
-    }
+    },
+    "ignorePatterns": [".eslintrc.js"]
 }
--- frontend/src/components/ProjectDownload.jsx
+++ frontend/src/components/ProjectDownload.jsx
@@ -1,45 +1,7 @@
 import { Alert, Center, Title } from "@mantine/core";
-import React, { useEffect, useRef, useState } from "react";
-import { saveAs } from 'file-saver';
+import React from "react";
 
-function ProjectDownload({ projectMetadata, projectSchema, onSubmit, onReady }) {
-  const projectRef = useRef({
-    ...projectMetadata,
-    project_schema: projectSchema
-  })
-  const [filename, setFilename] = useState(null)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    (async () => {
-      onSubmit()
-      setError('')
-      setFilename(null)
-      const { project_name } = projectRef.current
-      const filename = project_name + '.zip'
-      try {
-        const response = await fetch('http://localhost:8000/ws', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(projectRef.current)
-        })
-
-        if (!response.ok) {
-          throw new Error('API error')
-        }
-
-        const blob = await response.blob()
-        saveAs(blob, filename)
-        setFilename(filename)
-      } catch (e) {
-        setError('Something went wrong')
-      } finally {
-        onReady()
-      }
-    })()
-  }, [])
+function ProjectDownload({ filename, error }) {
 
   if (error) {
     return <Alert color="red">{error}</Alert>
